2aae845fccd02fddc133790bf8ee31d547db81f5
Nathanael <nblack@salesforce.com>
2016-02-04 11:22:40 -0800

    serve a redirect directly

--- bin/server.py
+++ bin/server.py
@@ -1,4 +1,6 @@
 import socket
+import SimpleHTTPServer
+import SocketServer
 from os import getenv, environ
 
 HOST, PORT = '', int(getenv('PORT', 5000))
@@ -6,30 +8,14 @@ LOGIN_URL = environ['FORCE_COM_PROTOTYPE_REDIRECT_URL']
 USERNAME = environ['FORCE_COM_PROTOTYPE_USERNAME']
 PASSWORD = environ['FORCE_COM_PROTOTYPE_PASSWORD']
 
-listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-listen_socket.bind((HOST, PORT))
-listen_socket.listen(1)
-print 'Serving HTTP on port %s ...' % PORT
-while True:
-    client_connection, client_address = listen_socket.accept()
-    request = client_connection.recv(1024)
-    print request
+class myHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+   def do_GET(self):
+       print self.path
+       self.send_response(303)
+       new_path = '%s?un=%s&pw=%s'%(LOGIN_URL, USERNAME, PASSWORD)
+       self.send_header('Location', new_path)
+       self.end_headers()
 
-    http_response = """\
-HTTP/1.1 200 OK
-
-<html>
-  <head>
-    <META http-equiv="refresh" content="0;{LOGIN_URL}?un={USERNAME}&amp;pw={PASSWORD}">
-  </head>
-  <body>
-    <h1>Redirecting to your Force.com Application</h1>
-  </body>
-</html>
-""".format(**locals())
-
-    print(http_response)
-
-    client_connection.sendall(http_response)
-    client_connection.close()
+handler = SocketServer.TCPServer(("", PORT), myHandler)
+print "serving at port {PORT}".format(PORT=PORT)
+handler.serve_forever()
