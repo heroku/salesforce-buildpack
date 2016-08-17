0aa46adb45ed5ad7b96422dd18cd8dd5077617c3
Chris Wall <cwall@salesforce.com>
2016-08-17 16:24:32 -0500

    Where is secret-tools (take 4)

--- lib/libs.sh
+++ lib/libs.sh
@@ -15,9 +15,6 @@ install_libsecret() {
     mkdir -p $BUILD_DIR/.profile.d
 
     export PATH="$HOME/.apt/usr/bin:$PATH"
-    export LD_LIBRARY_PATH="$HOME/.apt/usr/lib/x86_64-linux-gnu:$HOME/.apt/usr/lib/i386-linux-gnu:$HOME/.apt/usr/lib:$LD_LIBRARY_PATH"
-    export LIBRARY_PATH="$HOME/.apt/usr/lib/x86_64-linux-gnu:$HOME/.apt/usr/lib/i386-linux-gnu:$HOME/.apt/usr/lib:$LIBRARY_PATH"
-    export INCLUDE_PATH="$HOME/.apt/usr/include:$INCLUDE_PATH"
 
     ls -Llsrt $HOME/.apt/usr/bin
 }
