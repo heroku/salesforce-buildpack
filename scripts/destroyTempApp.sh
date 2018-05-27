869ce4a335f3f6c190fecc9dcce4269ca06f291d
Wade Wegner <wade.wegner@gmail.com>
2018-05-27 06:28:31 -0700

    Removed temp file

--- scripts/destroyTempApp.sh
+++
@@ -1,5 +0,0 @@
-heroku pipelines:destroy pipelineTempApp
-heroku apps:destroy -a devTempApp -c devTempApp
-heroku apps:destroy -a stagingTempApp -c stagingTempApp
-heroku apps:destroy -a prodTempApp -c prodTempApp
-rm -- "destroyTempApp.sh"
